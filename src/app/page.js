84986101f9e1864f68838d26b0e009883ca1d5dc
KevinCheng0212 <k1m8ch@gmail.com>
2023-11-29 14:08:41 +0800

    complete main line

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React,{useState} from "react";
+import { CodeBlock } from "react-code-blocks";
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+console.log(reverseString("Hello"));
 
 export default function Home() {
   return (
-    <main >
-     <h1>This is question :</h1>
-     
+    <main>
+      <h1>This is question :</h1>
+      <h3>Answer:</h3>
+      <div>
+        
+      </div>
     </main>
-  )
+  );
 }
