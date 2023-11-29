45b4ec6a9e8633be8854822b08a1788b60fd46b2
KevinCheng0212 <k1m8ch@gmail.com>
2023-11-29 14:21:20 +0800

    complete question-1

--- src/app/page.js
+++ src/app/page.js
@@ -10,10 +10,19 @@ console.log(reverseString("Hello"));
 export default function Home() {
   return (
     <main>
-      <h1>This is question :</h1>
+      <h1>This is question 1: JavaScript: 字串反轉</h1>
       <h3>Answer:</h3>
       <div>
-        
+      <CodeBlock
+          text={`function reverseString(str) {
+        return str.split("").reverse().join("");
+      }
+      console.log(reverseString("Hello"));`}
+          language="javascript"
+          showLineNumbers={true}
+          wrapLines={true}
+          codeBlock={true}
+        />
       </div>
     </main>
   );
