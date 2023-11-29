42a99782079c4e7c3fdab9b6cf84f40c7b8ba368
KevinCheng0212 <k1m8ch@gmail.com>
2023-11-29 14:33:37 +0800

    complete question-2

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,29 @@
 import React,{useState} from "react";
 import { CodeBlock } from "react-code-blocks";
 
+function filterNumbersGreaterThanFive(numbers) {
+  return numbers.filter((e) => e > 5);
+}
+const numbers = [2, 8, 4, 10, 1, 7];
+console.log(filterNumbersGreaterThanFive(numbers));
+
 export default function Home() {
   return (
     <main>
-      <h1>This is question :</h1>
+      <h1>This is question 2: JavaScript: 陣列過濾</h1>
       <h3>Answer:</h3>
       <div>
-      
+      <CodeBlock
+          text={`function filterNumbersGreaterThanFive(numbers) {
+        return numbers.filter((e) => e > 5);
+      }
+      const numbers = [2, 8, 4, 10, 1, 7];
+      console.log(filterNumbersGreaterThanFive(numbers));`}
+          language="javascript"
+          showLineNumbers={true}
+          wrapLines={true}
+          codeBlock={true}
+        />
       </div>
     </main>
   );
